963ab7edfbd8b06a39cce0b442b0a2baf5fcade2
Samarth Dave <samarthdave@users.noreply.github.com>
2016-07-09 16:34:12 +0000

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
-	validates :username, presence: true, length: {minimum: 3, maximum: 30}
-	validates :email, presence: true, length: {minimum: 7, maximum: 50}
+	validates :username, presence: true, length: {minimum: 3, maximum: 25},
+							   uniqueness: { case_sensitive: false }
+	VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: {minimum: 7, maximum: 50},
+							uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
 end
