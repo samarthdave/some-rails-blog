4f99243c3b05fe18a4f75dbe9e83c296c54c4740
Samarth Dave <samarthdave@users.noreply.github.com>
2016-07-09 14:25:29 +0000

    deploy

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+	def index
+		@articles = Article.all
+	end
+	def show
+	end
+	def new
+		@article = Article.new
+	end
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:success] = "Article saved successfully."
+			redirect_to article_path(@article)
+		else
+			flash[:danger] = "Looks like there was an error in saving your article."
+			render 'new'
+		end
+	end
+	def edit
+	end
+	def update
+		if @article.update(article_params)
+			flash[:success] = "Article successfully updated."
+			redirect_to article_path(@article)
+		else
+			flash[:warning] = "We couldn't update your article. Try again."
+			render 'edit'
+		end
+	end
+	def destroy
+		if @article.destroy
+			flash[:success] = "Article was deleted."
+			redirect_to articles_path
+		else
+			flash[:danger] = "Article was not deleted."
+			redirect_to article_path(@article)
+		end
+	end
+	private
+	def set_article
+		@article = Article.find(params[:id])
+	end
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+end
